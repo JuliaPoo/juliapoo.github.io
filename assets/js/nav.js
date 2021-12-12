04530714d1bb6af89cbdc5f192a38ea9b733bcec
Julia <julia.poo.poo.poo@gmail.com>
2021-12-12 15:15:05 +0800

    Should stop working on the nav bar

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,8 +1,17 @@
+
+// Dynamically resizes the nav bar.
 window.addEventListener('scroll', (event) => {
+
+    // Reduce size of nav
     let icon = document.getElementsByClassName("nav-icon")[0];
     let em = parseFloat(getComputedStyle(icon).fontSize);
-    let sz = Math.max(2.5*em, 5*em-window.scrollY) + 10;
+    let sz = Math.max(3*em, 5*em-window.scrollY*0.2) + 10;
     let sz_str = sz.toString() + "px";
     icon.style.height = sz_str; 
     icon.style.width = sz_str; 
+
+    // Make it disappear
+    let opacity = 4 - window.scrollY / (5*em);
+    let nav = document.getElementsByClassName("nav-container")[0];
+    nav.style.opacity = opacity.toString();
 });
