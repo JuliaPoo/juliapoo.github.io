e6cb5e5a6c0ae1c15ce804696535fe0658b8ed97
Julia <julia.poo.poo.poo@gmail.com>
2022-01-11 09:41:17 +0800

    Converted mp4 to h624 format

--- assets/art/2020-01-15-dot-splash/a.bat
+++ assets/art/2020-01-15-dot-splash/a.bat
@@ -1,11 +1,11 @@
 ::ffmpeg -y -i 1.gif -filter_complex "scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=16[p];[s1][p]paletteuse=dither=bayer" _preview.gif
 
-ffmpeg -i 1.gif -vframes 1 1-preview.png
-ffmpeg -i 2.gif -vframes 1 2-preview.png
-ffmpeg -i 3.gif -vframes 1 3-preview.png
-ffmpeg -i 4.gif -vframes 1 4-preview.png
+::ffmpeg -i 1.gif -vframes 1 1-preview.png
+::ffmpeg -i 2.gif -vframes 1 2-preview.png
+::ffmpeg -i 3.gif -vframes 1 3-preview.png
+::ffmpeg -i 4.gif -vframes 1 4-preview.png
 
-ffmpeg -i 1.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 1-com.mp4
-ffmpeg -i 2.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 2-com.mp4
-ffmpeg -i 3.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 3-com.mp4
-ffmpeg -i 4.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 4-com.mp4
+ffmpeg -i 1.gif -vcodec libx264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 1-com.mp4
+ffmpeg -i 2.gif -vcodec libx264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 2-com.mp4
+ffmpeg -i 3.gif -vcodec libx264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 3-com.mp4
+ffmpeg -i 4.gif -vcodec libx264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 4-com.mp4
