3b93599c7027da8e659325dea31d2fb16d8b1c1f
Julia <julia.poo.poo.poo@gmail.com>
2022-01-11 10:29:39 +0800

    Fix: more video stuff

--- assets/art/2020-01-15-dot-splash/a.bat
+++ assets/art/2020-01-15-dot-splash/a.bat
@@ -5,7 +5,7 @@
 ::ffmpeg -i 3.gif -vframes 1 3-preview.png
 ::ffmpeg -i 4.gif -vframes 1 4-preview.png
 
-ffmpeg -i 1.gif -vcodec libx264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 1-com.mp4
-ffmpeg -i 2.gif -vcodec libx264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 2-com.mp4
-ffmpeg -i 3.gif -vcodec libx264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 3-com.mp4
-ffmpeg -i 4.gif -vcodec libx264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 4-com.mp4
+ffmpeg -i 1.gif -vcodec h264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 1-com.mp4
+ffmpeg -i 2.gif -vcodec h264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 2-com.mp4
+ffmpeg -i 3.gif -vcodec h264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 3-com.mp4
+ffmpeg -i 4.gif -vcodec h264 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" 4-com.mp4
